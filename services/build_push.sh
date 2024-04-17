8d70c5406e85618f24bd70b44dd45ff1ddf5e421
zwarmex <jpollart@gmail.com>
2024-04-17 09:17:43 +0000

    CDN completed

--- services/build_push.sh
+++ services/build_push.sh
@@ -6,29 +6,29 @@ default_version="3"
 version=${1:-"$default_version"}
 
 
-docker build -t xdubruille/woody_api:"$version" api 
-docker tag xdubruille/woody_api:"$version" xdubruille/woody_api:latest
+docker build -t zwarmex/woody_api:"$version" api 
+docker tag zwarmex/woody_api:"$version" zwarmex/woody_api:latest
 
-docker build -t xdubruille/woody_rp:"$version" reverse-proxy
-docker tag xdubruille/woody_rp:"$version" xdubruille/woody_rp:latest
+docker build -t zwarmex/woody_rp:"$version" reverse-proxy
+docker tag zwarmex/woody_rp:"$version" zwarmex/woody_rp:latest
 
-docker build -t xdubruille/woody_database:"$version" database
-docker tag xdubruille/woody_database:"$version" xdubruille/woody_database:latest
+docker build -t zwarmex/woody_database:"$version" database
+docker tag zwarmex/woody_database:"$version" zwarmex/woody_database:latest
 
-docker build -t xdubruille/woody_front:"$version" front
-docker tag xdubruille/woody_front:"$version" xdubruille/woody_front:latest
+docker build -t zwarmex/woody_front:"$version" front
+docker tag zwarmex/woody_front:"$version" zwarmex/woody_front:latest
 
 
 # avec le "set -e" du début, je suis assuré que rien ne sera pushé si un seul build ne c'est pas bien passé
 
-docker push xdubruille/woody_api:"$version"
-docker push xdubruille/woody_api:latest
+docker push zwarmex/woody_api:"$version"
+docker push zwarmex/woody_api:latest
 
-docker push xdubruille/woody_rp:"$version"
-docker push xdubruille/woody_rp:latest
+docker push zwarmex/woody_rp:"$version"
+docker push zwarmex/woody_rp:latest
 
-docker push xdubruille/woody_front:"$version"
-docker push xdubruille/woody_front:latest
+docker push zwarmex/woody_front:"$version"
+docker push zwarmex/woody_front:latest
 
-docker push xdubruille/woody_database:"$version"
-docker push xdubruille/woody_database:latest
+docker push zwarmex/woody_database:"$version"
+docker push zwarmex/woody_database:latest
